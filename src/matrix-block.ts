30998ce7ef0c253217c3f6b8f8d06ecccbaaa552
kmsheng <kmsh3ng@gmail.com>
2018-04-15 18:01:26 +0800

    add isFadingOut prop to MatrixBlock

--- src/matrix-block.ts
+++ src/matrix-block.ts
@@ -4,6 +4,8 @@ export default class MatrixBlock {
   label: string;
   isPseudo: boolean = false;
 
+  isFadingOut: boolean = false;
+
   constructor(id = 0, label = 'empty', isPseudo = false) {
     this.id = id;
     this.label = label;
@@ -14,10 +16,16 @@ export default class MatrixBlock {
     this.id = 0;
     this.label = 'empty';
     this.isPseudo = false;
+    this.isFadingOut = false;
     return this;
   }
 
   isEmpty() {
     return ('empty' === this.label) && (0 === this.id);
+  }
+
+  setFadingOut() {
+    this.isFadingOut = true;
+    return this;
   }
 }
